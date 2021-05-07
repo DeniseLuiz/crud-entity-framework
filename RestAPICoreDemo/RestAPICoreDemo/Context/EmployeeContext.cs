7753947c0e2cf853a0e20139fc366d681dd05843
DeniseLuiz <denisesantos.luiz@usp.br>
2021-05-06 22:43:23 -0300

    DbContext

---
+++ RestAPICoreDemo/RestAPICoreDemo/Context/EmployeeContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestAPICoreDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPICoreDemo.Context
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
