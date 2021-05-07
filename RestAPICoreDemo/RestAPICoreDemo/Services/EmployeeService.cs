0a723c5f910fd94416dd58a61e2261cfa28b0622
DeniseLuiz <denisesantos.luiz@usp.br>
2021-05-06 23:05:15 -0300

    Services

---
+++ RestAPICoreDemo/RestAPICoreDemo/Services/EmployeeService.cs
@@ -0,0 +1,54 @@
+﻿using RestAPICoreDemo.Context;
+using RestAPICoreDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPICoreDemo.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public EmployeeContext _employeeDbContext;
+
+        public EmployeeService(EmployeeContext employeeDbContext)
+        {
+            _employeeDbContext = employeeDbContext;
+        }
+        public Employee AddEmployee(Employee employee)
+        {
+            _employeeDbContext.Employees.Add(employee);
+            _employeeDbContext.SaveChanges();
+            return employee;
+        }
+
+        public string DeleEmploye(int id)
+        {
+            var employee = _employeeDbContext.Employees.FirstOrDefault(x => x.Id == id);
+            
+            if(employee == null) 
+            {
+                return "Employee não encontrado.";
+            }
+            _employeeDbContext.Remove(employee);
+            _employeeDbContext.SaveChanges();
+            return "Employee Deletado com sucesso.";
+        }
+
+        public Employee GetEmployeeId(int id)
+        {
+            return _employeeDbContext.Employees.Find(id);
+        }
+
+        public List<Employee> GetEmployees()
+        {
+            return _employeeDbContext.Employees.ToList();
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            _employeeDbContext.Employees.Update(employee);
+            _employeeDbContext.SaveChanges();
+        }
+    }
+}
---
+++ RestAPICoreDemo/RestAPICoreDemo/Services/IEmployeeService.cs
@@ -0,0 +1,21 @@
+﻿using RestAPICoreDemo.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPICoreDemo.Services
+{
+    public interface IEmployeeService
+    {
+        Employee AddEmployee(Employee employee);
+
+        List<Employee> GetEmployees();
+
+        void UpdateEmployee(Employee employee);
+
+        string DeleEmploye(int id);
+
+        Employee GetEmployeeId(int id);
+    }
+}
