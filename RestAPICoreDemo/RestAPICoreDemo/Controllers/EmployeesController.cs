edd10b32fcf18e9345160a046b1f86910eb949e7
DeniseLuiz <denisesantos.luiz@usp.br>
2021-05-15 19:27:16 -0300

    controller

---
+++ RestAPICoreDemo/RestAPICoreDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestAPICoreDemo.Model;
+using RestAPICoreDemo.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestAPICoreDemo.Controllers
+{
+    [Route("v1/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        EmployeeService _employeeService;
+
+        public EmployeesController(EmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Employee> GetEmployee()
+        {
+            return _employeeService.GetEmployees();
+        }
+    }
+}
